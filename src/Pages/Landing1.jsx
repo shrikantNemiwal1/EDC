d00a41fa1cafab775c8a9e77d1bbdeddb9765bc4
Sumit063 <aspp775@gmail.com>
2023-04-17 19:02:09 +0530

    initial commit: Landing page 1

---
+++ src/Pages/Landing1.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Landing1 = () => {
+  return (
+    <div>
+        <main>
+            <form>
+                <input style={{padding:20 , border: "1px solid rgba(0,0,0,0.4)"}}
+                type="text"
+                placeholder="Enter"
+                />
+            </form>
+        </main>
+    </div>
+  )
+}
+
+export default Landing1
